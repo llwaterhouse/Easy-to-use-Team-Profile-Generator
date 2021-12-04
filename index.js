d156cb4d0b42afb06bba9c0a3ab04ce79a86d93d
Linda LW <lindalw03@gmail.com>
2021-12-04 11:25:46 -0500

    made a few styling changes to styles.css and am now using package chalk to change color of console.log output

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
+const chalk = require('chalk');
 
 const OUTPUT_DIR = path.resolve(__dirname, 'output');
 const outputPath = path.join(OUTPUT_DIR, 'team.html');
@@ -267,8 +268,8 @@ function appMenu() {
 			fs.mkdirSync(OUTPUT_DIR);
 		}
 		fs.writeFileSync(outputPath, render(teamMembers), 'utf-8');
-		// console.log('%c ' + `Success! Please view your file at ${outputPath}.`, `color:#FFA500`);
-		console.log(`Success! Please view your file at ${outputPath}.`);
+		//  console.log('%c ' + `Success! Please view your file at ${outputPath}.`, 'color:#FFA500');
+		console.log(chalk.green(`Success! Please view your file at ${outputPath}.`));
 	}
 
 	createManager();
--- src/page-template.js
+++ src/page-template.js
@@ -5,7 +5,7 @@ const generateTeam = (team) => {
 	// create the manager html
 	const generateManager = (manager) => {
 		return `
-        <div class="card employee-card">
+        <div class="card employee-card manager-card">
         <div class="card-header">
             <h2 class="card-title">${manager.getName()}</h2>
             <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
@@ -26,7 +26,7 @@ const generateTeam = (team) => {
 		// fill out the template literal with the engineer's specific info and return
 
 		return `
-        <div class="card employee-card">
+        <div class="card employee-card engineer-card">
         <div class="card-header">
             <h2 class="card-title">${engineer.getName()}</h2>
             <h3 class="card-title"><i class="fas fa-user-secret mr-2"></i>${engineer.getRole()}</h3>
@@ -46,7 +46,7 @@ const generateTeam = (team) => {
 	const generateIntern = (intern) => {
 		// fill out template literal and return
 		return `
-        <div class="card employee-card">
+        <div class="card employee-card intern-card">
         <div class="card-header">
             <h2 class="card-title">${intern.getName()}</h2>
             <h3 class="card-title"><i class="fas fa-school mr-2"></i>${intern.getRole()}</h3>
@@ -90,7 +90,7 @@ module.exports = (team) => {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>My Team</title>
+    <title>My Fabulous Team</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
@@ -101,7 +101,7 @@ module.exports = (team) => {
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
+                <h1 class="text-center">My Fabulous Team</h1>
             </div>
         </div>
     </div>
